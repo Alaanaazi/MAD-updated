e66bc9ce64949e81313567745e321f022c6a6e4f
Arosh Segar <arosh.segar@gmail.com>
2020-09-04 22:29:22 +0530

    MAD initial commit

---
+++ app/src/main/java/com/example/madprojecttest/AppBar.java
@@ -0,0 +1,33 @@
+package com.example.madprojecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class AppBar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_bar);
+
+        TextView Title=(TextView) findViewById(R.id.ToolbarTitle);
+        Title.setText("News");
+
+        ImageButton menu = (ImageButton) findViewById(R.id.menu);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent1 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/madprojecttest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.madprojecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import static android.view.View.Z;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CardView news= (CardView) findViewById(R.id.NewsCardView);
+        news.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),AppBar.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
