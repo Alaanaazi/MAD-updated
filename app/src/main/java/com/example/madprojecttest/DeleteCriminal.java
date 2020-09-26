aaa714ba6f5d0784fd2adb386c65257ae92bb4e5
sabesan1998 <sabesanganeshalingam@gmail.com>
2020-09-26 15:08:17 +0530

    ploice dashboard

---
+++ app/src/main/java/com/example/madprojecttest/DeleteCriminal.java
@@ -0,0 +1,14 @@
+package com.example.madprojecttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteCriminal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_criminal);
+    }
+}
