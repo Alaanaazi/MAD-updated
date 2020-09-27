3b2ec2409230cc67dccdd6ac206399e81023190b
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2020-09-28 01:27:08 +0530

    Firebase linked

--- app/src/main/java/com/example/madprojecttest/MainActivity.java
+++ app/src/main/java/com/example/madprojecttest/MainActivity.java
@@ -24,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        CardView alertzone =findViewById(R.id.AlertZoneCardView);
+        alertzone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),AlertZone.class);
+                startActivity(intent);
+            }
+        });
     }
 }
