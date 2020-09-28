efa1cd64e883812cb3a3a15562bfa44d39276c91
IT19125794 <it19125794@my.sliit.lk>
2020-09-28 18:14:26 +0530

    Add Emergency Contacts created

---
+++ app/src/main/java/com/example/madprojecttest/AddEmergency.java
@@ -0,0 +1,14 @@
+package com.example.madprojecttest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddEmergency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_emergency);
+    }
+}
