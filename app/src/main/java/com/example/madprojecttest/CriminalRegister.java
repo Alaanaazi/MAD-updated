4e9f53a3bc6260cd42d5f4a9616115cd28ac9176
Arosh Segar <arosh.segar@gmail.com>
2020-09-29 19:13:58 +0530

    Criminal registration

--- app/src/main/java/com/example/madprojecttest/CriminalRegister.java
+++ app/src/main/java/com/example/madprojecttest/CriminalRegister.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Police_Registration extends AppCompatActivity {
+public class CriminalRegister extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_police__registration);
+        setContentView(R.layout.activity_criminal_register);
     }
 }
