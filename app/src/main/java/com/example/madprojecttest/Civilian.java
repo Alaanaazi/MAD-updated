9c0378bf2a60c34a5d2a28ef5b2e295839df4f1a
Mathursan-007 <63546506+Mathursan-007@users.noreply.github.com>
2020-09-29 16:52:51 +0530

    Police,Civilian insert done

---
+++ app/src/main/java/com/example/madprojecttest/Civilian.java
@@ -0,0 +1,51 @@
+package com.example.madprojecttest;
+
+public class Civilian {
+
+    private String name;
+    private String NIC;
+    private String email;
+    private Integer phone;
+    private String pwd;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNIC() {
+        return NIC;
+    }
+
+    public void setNIC(String NIC) {
+        this.NIC = NIC;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+}
